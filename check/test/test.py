c798d36f394e1ec7bf07201d59aa781359fac6b0
Kavimani-S <112235556+Kavimani-S@users.noreply.github.com>
2024-05-14 16:28:29 +0530

    Update test.py

--- check/test/test.py
+++ check/test/test.py
@@ -5,12 +5,12 @@ import unittest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from dummytest.main import Calculator
 
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        calc = Calculator()
-        self.assertEqual(calc.add(1, 2), 3)
-        self.assertEqual(calc.add(-1, 1), 0)
-        self.assertEqual(calc.add(0, 0), 0)
+class TestAddition(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
 
-if __name__ == "__main__":
+    def test_addition_with_negative_numbers(self):
+        self.assertEqual(-1 + 1, 0)
+
+if __name__ == '__main__':
     unittest.main()
